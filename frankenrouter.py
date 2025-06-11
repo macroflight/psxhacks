d8143261705e695021d853ae5bd6038877c25345
Mats Kronberg <mats.kronberg.game@gmail.com>
2025-06-11 18:37:03 +0200

    frankenrouter: minor cleanup

--- frankenrouter.py
+++ frankenrouter.py
@@ -44,7 +44,7 @@ async def handle_new_psx_client(reader, writer):
     # Problem with FrankenUSB: psx.get(FltControls) returns None until
     # someone else has moved the flight controls. Works with a direct
     # connection though.
-    
+
     # Send the id and version to the client
     for variable in ['id', 'version']:
         if variable in current_conn:
@@ -57,7 +57,7 @@ async def handle_new_psx_client(reader, writer):
 
     line = f"layout=1\n" # fake
     writer.write(line.encode())
-            
+
     # Send the lexicon to the client (at least FrankenUSB needs this)
     # FIXME: handle this for a client that connects before the main
     # server, or reconnect.
@@ -73,14 +73,14 @@ async def handle_new_psx_client(reader, writer):
     writer.write(line.encode())
     line = f"load3\n"
     writer.write(line.encode())
-        
+
     # Send a "start" to the server to force it to resend the startup
     # info
     await write_to_psx_server("start\n".encode(), f"forced startup due to {addr}")
-    
+
     # Send a "bang" to force resending of all non-Delta variables
     await write_to_psx_server("bang\n".encode(), f"forced bang due to {addr}")
-    
+
     # Wait for client input
     while True:
         # We know the protocol is text-based, so we can use readline()0
@@ -92,7 +92,7 @@ async def handle_new_psx_client(reader, writer):
             print(f"Received EOF from client {addr} - closing connection. clients={clients.keys()}")
             return
         print(f"Received {len(message)} bytes from {addr}, sending to PSX: {data}")
-        print(f"DEBUG: type of data is {type(data)}")
+        # print(f"DEBUG: type of data is {type(data)}")
         await write_to_psx_server(data, addr)
         await writer.drain()
 
@@ -112,18 +112,18 @@ async def write_to_connected_clients(data):
     if len(clients) < 1:
         # print("No connected clients, cannot write data")
         return
-    for addr, client in clients.items():       
+    for addr, client in clients.items():
         # print(f"Writing {len(data)} bytes to client {addr}")
         # print(f"PSX => {addr}: {data.decode().rstrip()}")
         await client['writer'].drain()
         client['writer'].write(data)
-        
+
 async def write_to_psx_server(data, client_addr):
     global psx_server
     if psx_server is None:
         print(f"No PSX server connection, cannot send client data: {data}")
         return
-    print(f"DEBUG: data from client: =>>{data}<==")
+    # print(f"DEBUG: data from client: =>>{data}<==")
     print(f"PSX <= {client_addr}: {data.decode().rstrip()}")
     writer = psx_server[1]
     writer.write(data)
@@ -160,7 +160,7 @@ async def psx_server_connection():
                 await asyncio.sleep(1)
                 break
 
-            print(f'Received from PSX: {data.decode().rstrip()}')
+            # print(f'Received from PSX: {data.decode().rstrip()}')
 
             # Store various things that we get e.g on initial
             # connection and that we might need later.
@@ -178,9 +178,9 @@ async def psx_server_connection():
                 current_conn['id'] = value
             elif key == 'version':
                 current_conn['version'] = value
-                
+
             await write_to_connected_clients(data)
-    
+
 async def main():
     print("Setting up proxy server")
     proxy_server = await asyncio.start_server(handle_new_psx_client, '127.0.0.1', 10748)
