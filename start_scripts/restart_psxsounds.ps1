02630c8e1cdf396edda94ab397bb62d8baf2dbee
Mats Kronberg <mats.kronberg.game@gmail.com>
2026-07-08 15:41:19 +0200

    start_scripts: force PSXSounds to connect to the correct router (slave)

--- start_scripts/restart_psxsounds.ps1
+++ start_scripts/restart_psxsounds.ps1
@@ -7,6 +7,10 @@ $configPath = "$PsxSoundsDir\Config.xml"
 $xml = New-Object System.Xml.XmlDocument
 $xml.Load($configPath)
 $xml.SelectSingleNode("//RB211").InnerText = $PsxSoundsRb211
+# PSXSounds is started from startsim_slave.ps1, so it must connect to the
+# SLAVE sim's router port.
+$xml.SelectSingleNode("//PSXServerIP").InnerText = "127.0.0.1"
+$xml.SelectSingleNode("//PSXPort").InnerText = "$FrankenrouterSlavePort"
 $xml.Save($configPath)
 
 Start-Process -WorkingDirectory $PsxSoundsDir "$PsxSoundsDir\PSXSounds.exe"
